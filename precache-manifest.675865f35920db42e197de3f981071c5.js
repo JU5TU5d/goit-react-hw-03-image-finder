0d467e7ffa383e32121851bedcff824013eed9fd
JU5TU5d <vladislav261100@gmail.com>
2019-10-23 08:33:36 +0300

    Updates

---
+++ precache-manifest.675865f35920db42e197de3f981071c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6826115a2d4963e7056a964c4da564",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "40ec269e3a7d1d676c7d",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7de83bd9.chunk.css"
+  },
+  {
+    "revision": "38c856d16437b5bba0c7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d1ed3f7d.chunk.js"
+  },
+  {
+    "revision": "40ec269e3a7d1d676c7d",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.8ec58382.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
